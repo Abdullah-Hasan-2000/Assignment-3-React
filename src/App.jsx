68aaca1159e89d9b7150c813869bd92b79c51d4a
Allukaparatha <ahjhrhah3@gmail.com>
2025-07-08 21:22:09 +0500

    2 more components added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Navbar_1 from './components/Navbar_1/Navbar_1.jsx';
 import Navbar_Search from './components/Navbar_Search/Navbar_Search.jsx';
 import Carousel_Daraz from './components/Carousel_1/Carousel_1.jsx';
 import Cards from './components/Cards_D/Cards_D.jsx';
+import Try_App from './components/Try_App/Try_App.jsx';
 
 import { products } from './Data.jsx';
 
@@ -26,16 +27,15 @@ function App() {
           <Col xs={10} >
             <Carousel_Daraz />
           </Col>
-          <Col xs={2}>
-            <div></div>
+          <Col xs={2} style={{ margin: "0", padding: "0"}}>
+            
+              <Try_App />
+            
           </Col>
         </Row>
 
         <div>
           <h1>Just For You</h1>
-          <div style={{ display: "flex", flexWrap: "wrap" }}>
-
-          </div>
           <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", rowGap: "20px"}}>
             {products.map((e, i) => {
               return <Cards imgSrc={e.image} title={e.title} price={e.price} />;
--- src/components/Try_App/Try_App.jsx
+++ src/components/Try_App/Try_App.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import logo_image from '../../assets/logo_only.webp';
+import styles from './Try_App.module.css';
+import { FaStar } from "react-icons/fa6";
+import delivery_logo from '../../assets/delivery_logo.webp';
+import exclusive_logo from '../../assets/exclusive_logo.webp';
 
 const Try_App = () => {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.headingContainer}>
+        <div className={styles.logoContainer}>
+          <img style={{backgroundColor: 'transparent'}} width='100%' src={logo_image} alt="" />
+        </div>
+        <h6>TRY DARAZ APP</h6>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.textRating}>
+          <FaStar style={{ fill: 'white'}} />
+          <span>4.8 Rated</span>
+        </div>
+        <div className={styles.textDownload}>
+          <p>Download the App now</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <div>
+            <img src={delivery_logo} alt="" />
+            <p>Free Shipping</p>
+          </div>
+          <div>
+            <img src={exclusive_logo} alt="" />
+            <p>Exclusive Vouchers</p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
 
